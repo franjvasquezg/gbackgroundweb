ff6793643221beae038f4dc2f1c320e692140993
Francisco Vasquez Guerrero <fvasquez@cnti.gob.ve>
2011-05-11 14:19:03 -0430

    Versión inicial de canaima-fondo-institucional-1.0 para Canaima GNU/Linux

---
+++ debian/ejemplos/canaima-fondo-institucional.default.ex
@@ -0,0 +1,10 @@
+# Defaults for canaima-fondo-institucional initscript
+# sourced by /etc/init.d/canaima-fondo-institucional
+# installed at /etc/default/canaima-fondo-institucional by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/ejemplos/menu.ex
@@ -0,0 +1,2 @@
+?package(canaima-fondo-institucional):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="canaima-fondo-institucional" command="/usr/bin/canaima-fondo-institucional"
